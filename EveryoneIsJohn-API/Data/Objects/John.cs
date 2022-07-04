227984168c17831f4b2f704cf83822ddca127e88
Oscar Davies <oscar.william.davies@outlook.com>
2022-07-04 14:42:14 +0100

    Create John & Score

---
+++ EveryoneIsJohn-API/Data/Objects/John.cs
@@ -0,0 +1,42 @@
+﻿namespace EveryoneIsJohn_API.Data.Objects
+{
+    public class John : DataObject
+    {
+        #region Constructors
+
+        public John(User user, string johnsName = "John")
+        {
+            Creator = user.Identifier;
+            Name = johnsName;
+            scores = new List<Score>();
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public int Creator { get; private set; }
+
+        public string Name { get; set; }
+
+        public List<int> pendingPlayers { get; set; }
+
+        public List<Score> scores { get; set; }
+
+        #endregion Properties
+
+        #region Methods
+
+        public bool GrantPending(int userId)
+        {
+            if (pendingPlayers.Remove(userId))
+            {
+                scores.Add(new Score(userId));
+                return true;
+            }
+            return false;
+        }
+
+        #endregion Methods
+    }
+}
---
+++ EveryoneIsJohn-API/Data/Objects/Score.cs
@@ -0,0 +1,26 @@
+﻿namespace EveryoneIsJohn_API.Data.Objects
+{
+    public class Score
+    {
+        #region Constructors
+
+        public Score(int user)
+        {
+            User = user;
+            Level1 = 0;
+            Level2 = 0;
+            Level3 = 0;
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public int Level1 { get; set; }
+        public int Level2 { get; set; }
+        public int Level3 { get; set; }
+        public int User { get; set; }
+
+        #endregion Properties
+    }
+}
